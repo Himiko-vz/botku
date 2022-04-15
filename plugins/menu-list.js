5299340f1c0fca056511841395de1d59d0f8dc2f
Himiko-vz <103635626+Himiko-vz@users.noreply.github.com>
2022-04-15 19:08:00 +0700

    Update menu-list.js

--- plugins/menu-list.js
+++ plugins/menu-list.js
@@ -414,8 +414,8 @@ let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(
            hydratedFooterText: wm,
            hydratedButtons: [{
             urlButton: {
-               displayText: 'SOURCE CODE',
-               url: 'https://youtu.be/_HmhBUN7B-4'
+               displayText: 'Instagram 📍',
+               url: 'https://www.instagram.com/Kevinafsar_'
              }
 
            },
